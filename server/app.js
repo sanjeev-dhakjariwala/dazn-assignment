affde5dd6ddb8dcceff6070bfe1d9836181c583b
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-03-08 23:30:48 +0530

    error middlewares added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const helmet = require("helmet")
+const helmet = require('helmet');
+const { notFound, errorHandler } = require('../middlewares/error.handler');
 
 app.use(express.json({ limit: '50mb' }));
-app.use(helmet())
+app.use(helmet());
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
