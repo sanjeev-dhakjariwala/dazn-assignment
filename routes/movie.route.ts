5eb33123986e95e20f2304bed7e2b41200643955
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-03-09 03:03:48 +0530

    Routes added

--- routes/movie.route.ts
+++ routes/movie.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
-
+import {
+  addMovies,
+  getMovies,
+  searchMovies,
+  deleteMovie,
+  updateMovies
+} from '../controllers/movie.controller';
 const router = express.Router();
 
-router.route('/movies').get().post();
-router.route('/search').get();
-router.route('/movies/:id').put().patch()
+router.route('/movies').get(getMovies).post(addMovies);
+router.route('/search').get(searchMovies);
+router.route('/movies/:id').put(updateMovies).delete(deleteMovie);
 
 export default router;
