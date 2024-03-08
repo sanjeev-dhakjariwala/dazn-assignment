fc695835231cafd57a8e9c9db9ecd134eb62cd6c
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-03-09 01:38:57 +0530

    code refractored from .js to .ts

---
+++ middlewares/error.handler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.send({
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+export { notFound, errorHandler };
---
+++ server/app.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express';
+import helmet from 'helmet';
+import { notFound, errorHandler } from '../middlewares/error.handler';
+
+const app: Express = express();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Server started successfully!!!',
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
