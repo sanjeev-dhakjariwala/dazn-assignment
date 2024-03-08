9bf5f4c02d898a81d45e3c0dcb58dbd6f180b4ee
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-03-09 03:13:46 +0530

    Movies Schema updated

--- database/models/Movie.ts
+++ database/models/Movie.ts
@@ -1,22 +1,34 @@
+import { timeStamp } from 'console';
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface IMovie extends Document {
+  title: string;
   genre: string;
   rating: number;
   streamingLink: string;
 }
 
-const MovieSchema: Schema = new Schema({
-  genre: { type: String, required: true },
-  rating: {
-    type: Number,
-    required: true
+const MovieSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    genre: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true
+    },
+    streamingLink: {
+      type: String,
+      required: true
+    }
   },
-  streamingLink: {
-    type: String,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 const Movie = mongoose.model<IMovie>('Movie', MovieSchema);
-export default Movie
+export default Movie;
--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,14 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
 import { notFound, errorHandler } from '../middlewares/error.handler';
-import movieRoute from "../routes/movie.route"
+import movieRoute from '../routes/movie.route';
 
 const app: Express = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(helmet());
 
-
+app.use('/api', movieRoute);
 app.get('/', (req, res) => {
   res.status(200).json({
     success: true,
